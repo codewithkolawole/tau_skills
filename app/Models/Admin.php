5b2b535907b9c3177c527e623e7d30ade0d2082a
skyhackeR <jolayemidavid6@gmail.com>
2024-05-06 11:47:08 +0100

    laravel ui and multi-auth installation

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,10 +2,40 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use App\Notifications\AdminResetPassword;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Admin extends Model
+class Admin extends Authenticatable
 {
-    use HasFactory;
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * Send the password reset notification.
+     *
+     * @param  string  $token
+     * @return void
+     */
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new AdminResetPassword($token));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function () {
+  Route::get('/login', 'Admin\Auth\LoginController@showLoginForm')->name('login');
+  Route::post('/login', 'Admin\Auth\LoginController@login');
+  Route::post('/logout', 'Admin\Auth\LoginController@logout')->name('logout');
+
+  Route::get('/register', 'Admin\Auth\RegisterController@showRegistrationForm')->name('register');
+  Route::post('/register', 'Admin\Auth\RegisterController@register');
+
+  Route::post('/password/email', 'Admin\Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.request');
+  Route::post('/password/reset', 'Admin\Auth\ResetPasswordController@reset')->name('password.email');
+  Route::get('/password/reset', 'Admin\Auth\ForgotPasswordController@showLinkRequestForm')->name('password.reset');
+  Route::get('/password/reset/{token}', 'Admin\Auth\ResetPasswordController@showResetForm');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
